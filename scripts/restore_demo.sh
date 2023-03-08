e716c961d9654d36fa660e1935f4512d5b165c57
Gérard Dethier <gerard@logion.network>
2023-03-08 17:55:54 +0100

    feat: make restore_demo more realistic.
    logion-network/logion-internal#812

--- scripts/restore_demo.sh
+++ scripts/restore_demo.sh
@@ -10,16 +10,41 @@ function print_section()
     echo "*"
 }
 
-# Kill node1's private database
-print_section "Killing node1..."
-docker-compose stop private-database1
-docker exec -i logion-test_backup_manager1_1 /bin/sh -c 'rm -Rf /opt/logion-pg-backup-manager/db_data/*'
-docker-compose stop backup_manager1
-docker-compose rm -f private-database1
+# Retrieve journal (is sent by e-mail in production)
+print_section "Saving journal..."
+mkdir -p /tmp/work/
+JOURNAL=/tmp/work/journal.txt
+docker cp logion-test_backup_manager1_1:/opt/logion-pg-backup-manager/work/journal.txt $JOURNAL
 
-# Re-create node1's private database
-print_section "Restoring node1..."
+# Kill node1
+print_section "Killing node1..."
+docker-compose stop private-database1 node1 backup_manager1 backend1 frontend1
+docker-compose rm -f private-database1 node1 backup_manager1 backend1 frontend1
+docker volume rm logion-test_db_backup logion-test_db_data
+
+# Re-create a database
+print_section "Re-creating node1 database..."
 docker-compose up -d private-database1
 sleep 1 # Wait for postgres to be ready
-docker-compose start backup_manager1
-docker exec -i logion-test_backup_manager1_1 /bin/sh -c 'echo "Restore" > /opt/logion-pg-backup-manager/work/command.txt'
+
+# Put journal in manager's data volume
+print_section "Preparing node1 data restore..."
+docker run -d -it -v logion-test_db_backup:/opt/logion-pg-backup-manager/work --name logion-test_init-manager busybox sh
+docker cp $JOURNAL logion-test_init-manager:/opt/logion-pg-backup-manager/work/journal.txt
+docker stop logion-test_init-manager && docker rm logion-test_init-manager
+
+# Actually restore the data
+print_section "Restoring node1 data..."
+docker run --rm -v logion-test_db_backup:/opt/logion-pg-backup-manager/work -v $(pwd)/config/.pgpass:/root/.pgpass --env-file .backup.env --network logion-test_default -e RESTORE_AND_CLOSE=true logionnetwork/logion-pg-backup-manager:latest
+
+# Start services
+print_section "Starting node1 node and backend..."
+docker-compose up -d node1 backend1 frontend1 backup_manager1
+
+##################################################################################################
+# In production, someone should check now that the node is behaving well again before proceeding #
+##################################################################################################
+
+# Put manager back into automatic mode (restore forces manual mode)
+print_section "Resume automatic backup..."
+docker-compose exec backup_manager1 bash -c 'echo "Backup" > /opt/logion-pg-backup-manager/work/command.txt'
