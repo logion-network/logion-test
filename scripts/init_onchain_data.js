a5979f086c4386d23aca75a2b1ab0a3d6bb04809
Gérard Dethier <gerard@logion.network>
2023-06-06 15:40:56 +0200

    feat: add LO region.
    logion-network/logion-internal#914

--- scripts/init_onchain_data.js
+++ scripts/init_onchain_data.js
@@ -20,6 +20,7 @@ buildApiClass("ws://localhost:9944")
             Host: {
                 nodeId: "0x0024080112201ce5f00ef6e89374afb625f1ae4c1546d31234e87e3c3f51a62b91dd6bfa57df",
                 baseUrl: "http://localhost:8080",
+                region: "Europe",
             }
         })
         .signAndSend(alice);
@@ -31,6 +32,7 @@ buildApiClass("ws://localhost:9944")
             Host: {
                 nodeId: "0x002408011220dacde7714d8551f674b8bb4b54239383c76a2b286fa436e93b2b7eb226bf4de7",
                 baseUrl: "http://localhost:8081",
+                region: "Europe",
             }
         })
         .signAndSend(bob);
@@ -42,6 +44,7 @@ buildApiClass("ws://localhost:9944")
             Host: {
                 nodeId: "0x002408011220876a7b4984f98006dc8d666e28b60de307309835d775e7755cc770328cdacf2e",
                 baseUrl: "http://localhost:8082",
+                region: "Europe",
             }
         })
         .signAndSend(charlie);
