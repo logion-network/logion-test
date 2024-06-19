ab7b6adba5977ac9448feeb3b9650bdb3a7d01a0
Gérard Dethier <gerard@logion.network>
2024-06-19 08:36:48 +0200

    feat: use decentralized directory.
    logion-network/logion-internal#1292

--- scripts/init_data.sh
+++ scripts/init_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Directory
-docker compose exec -T directory-database psql -U postgres postgres < scripts/directory_data.sql
+docker compose exec -T private-database1 psql -U postgres postgres < scripts/llo_data_node1.sql
+docker compose exec -T private-database2 psql -U postgres postgres < scripts/llo_data_node2.sql
 
 # Authority list
 node ./scripts/init_onchain_data.js
